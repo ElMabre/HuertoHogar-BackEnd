a5151c4b4c42051f0e81626c341bf693546a489c
Matias Ignacio Guzman <mguzmanarza@gmail.com>
2025-11-18 15:18:18 -0300

    Se modificaron algunos valores para la integracion con AWS

--- ms-usuarios/src/main/java/cl/huertohogar/usuarios/config/SecurityConfiguration.java
+++ ms-usuarios/src/main/java/cl/huertohogar/usuarios/config/SecurityConfiguration.java
@@ -66,7 +66,7 @@ public class SecurityConfiguration {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+        configuration.setAllowedOrigins(List.of("http://localhost:3000, \"http://54.221.8.239:3000\""));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type", "Cache-Control"));
         configuration.setAllowCredentials(true);
